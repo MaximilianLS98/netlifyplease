9b42dc88f70378b13cb8ecddbe5e2f1c23470224
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2022-07-13 10:19:59 +0200

    tried to fix the styling as well

--- pages/index.js
+++ pages/index.js
@@ -13,26 +13,11 @@ export default function Home({ posts }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
       <div className="posts">
   {posts.map((p) => {
     return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
   })}
 </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
       <style jsx>{`
         .container {
           height: 100vh;
